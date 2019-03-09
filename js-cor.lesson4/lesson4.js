b66e85095302c8860db4cd3227854fdc66aa3f1d
goldenkamery <47122499+goldenkamery@users.noreply.github.com>
2019-03-09 20:48:27 +0200

    com1

---
+++ js-cor.lesson4/lesson4.js
@@ -0,0 +1,32 @@
+/*Обязательная часть ( 3 балла )
+Создайте объект, свойства которого описывают содержимое дамской сумочки
+Создайте метод объекта, позволяющий удалить что-то из сумочки
+Создайте метод объекта, позволяющий положить что-то в сумочку
+*/
+
+
+
+var womanBag = {
+	cosmetics: 'pomade',
+	phone: 'meizum3note',
+	accessories: 'mirror',
+	keys: 'branch',
+	documents: 'passport',
+
+	getThingInBag: function (thing) {
+						var thing = prompt ( "Что достаем из сумки?" );
+                        thing in womanBag ? delete womanBag[thing] : alert("В сумке такого нет");
+                   },
+
+	setThingInBag: function (thing1, subject) {
+						var thing1 = prompt ( "Что кладем в сумкy?" );
+						var subject =  prompt ( "Какой предмет?" )
+						womanBag[thing1] = subject;
+                   },
+};
+
+
+womanBag.getThingInBag();
+console.log(womanBag);
+womanBag.setThingInBag();
+console.log(womanBag);

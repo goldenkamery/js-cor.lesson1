4cb6f41cb8ae511da71c42c27db7bdc1e4e20766
goldenkamery <47122499+goldenkamery@users.noreply.github.com>
2019-04-02 10:45:59 +0300

    8/2

--- js-cor.lesson8/lesson8.js
+++ js-cor.lesson8/lesson8.js
@@ -17,10 +17,31 @@ tiktak.style = `
 for ( var sec = 0; sec < 100; sec++ ) {
 		( function ( arg ) {
 			return function () {
-				   		setTimeout (
-							() => tiktak.innerText = new Date ().toLocaleString ().split (', ')[1],
-							1000 * arg
-						)
-				   }
-		} ) ( sec ) () 
+				  setTimeout (
+				    () => tiktak.innerText = new Date ().toLocaleString ().split (', ')[1],
+				    1000 * arg
+				   )
+			      }
+		 } ) ( sec ) () 
 }
+
+
+
+/* Additionally
+Завершите код рекурсивной функции typeMessage так, чтобы при ее вызове на страницу выводился один символ в секунду
+
+var typeMessage = ( function ( velocity ) {
+    let container = document.getElementById ( "demo" ) ?
+        document.getElementById ( "demo" ) :
+        document.body.appendChild (
+            document.createElement ( "h3" )
+        )
+    container.style = `color: magenta;`
+    var index = 0
+    return function ( message ) {
+        ...
+    }
+})( 1 )
+
+typeMessage ( `Welcome to the hell` ) */
+

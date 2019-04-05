39067b0c923e236aff36e2b12e02eb1029faffc1
goldenkamery <47122499+goldenkamery@users.noreply.github.com>
2019-04-05 07:38:14 +0300

    k2

--- js-cor.lesson8/lesson8.js
+++ js-cor.lesson8/lesson8.js
@@ -7,13 +7,7 @@
 var tiktak = document.body.appendChild (
 		document.createElement ( 'h2' )
 );
-tiktak.style = `
-	  text-align: center;
-	  color: #A52A2A;
-    margin: auto;
-    padding: auto;
-	  font-size: 200px;
-`
+
 for ( var sec = 0; sec < 100; sec++ ) {
 		( function ( arg ) {
 			return function () {
@@ -45,3 +39,72 @@ var typeMessage = ( function ( velocity ) {
 
 typeMessage ( `Welcome to the hell` ) */
 
+var typeMessage = ( function ( velocity ) {
+    let container = document.getElementById ( "demo" ) ?
+        document.getElementById ( "demo" ) :
+        document.body.appendChild (
+            document.createElement ( "h3" )
+        )
+    container.style = `color: magenta;`
+    var index = 0
+    return function ( message ) {
+        var number = index
+        setTimeout (
+            () => container.textContent += message [ number ],
+            1000 * velocity * number
+        )
+        index++ < message.length - 1 ?
+            arguments.callee ( message ) : null
+    }
+})( 1 )
+
+typeMessage ( `Welcome to the hell` )
+
+
+/*  Additionally
+Нужно сделать так, чтобы у всех экзепляров, созданных конструктором User, был унаследованный метод counter(),
+который считает созданные конструктором экземпляры
+Каждый вызов конструктора увеличивает счетчик экземпляров
+на 1 и помещать новое значение счетчика в свойство id созданного экземпляра: 
+
+function User ( name ) {
+    this.name = name
+    this.id = this.counter()
+}
+
+...
+
+var users = [
+    new User ( "Семен" ),
+    new User ( "Антон" ),
+    new User ( "Демьян" ),
+    new User ( "Василий" )
+]
+:warning: После выполнения кода:
+
+users[1].id = users[1].counter()
+свойство id экземпляра не должно измениться, так же, как и значение счетчика */
+
+
+function User ( name ) {
+    this.name = name
+    this.id = this.counter()
+}
+
+User.prototype.counter = (
+    function(){
+        var id = 0
+        return function(){
+            return typeof this.id === 'number' ? this.id: id++
+        }
+    }
+)()
+
+var users = [
+    new User ( "Семен" ),
+    new User ( "Антон" ),
+    new User ( "Демьян" ),
+    new User ( "Василий" )
+];
+
+users[1].id = users[1].counter();

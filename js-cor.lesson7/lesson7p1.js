dd34ac6363eabfaf89b9716faa2e156542edfb3f
goldenkamery <47122499+goldenkamery@users.noreply.github.com>
2019-04-05 07:03:20 +0300

    к2

--- js-cor.lesson7/lesson7p1.js
+++ js-cor.lesson7/lesson7p1.js
@@ -10,23 +10,41 @@ var p = document.body.appendChild(
 );
 p.innerText = 'Hello, My Dear Friend! Click here!';
 
-var picture = document.createElement( 'img' );
+var picture = new Image();
 picture.setAttribute ( 'src', 'https://telegraf.com.ua/files/2016/11/443276.jpg' );
-picture.style = `display: none`;
 
-p.onclick = ( event ) => {
-    document.body.appendChild(picture);
-    picture.style = `width: 100px`
-}
 
-picture.onmouseover = ( event ) => {
- picture.style = `
-       transition: all 2s;
-       width: 200px;
+p.onclick = function ( event ) {
+
+    ( arguments[0] && arguments[0].nodeType === 1 ? arguments[0] : document.body )
+        .appendChild( this )
+            .style = `width: 100px`
+
+}.bind ( picture )
+
+picture.onmouseover = event => 
+    event.target.style = `
+        transition: all 2s;
+        width: 200px;
     `
-}
 
-picture.onclick = ( event ) => {
-    document.body.removeChild(picture)
-}
+picture.onclick = event => 
+    event.target
+        .parentNode
+            .removeChild( event.target )
 
+/* Приобретаем правильные привычки и делаем выводы! */
+/* Поработаем с контейнером для вставки элемента
+А вдруг нужно будет вставить не в document.body, а в section какой-нибудь? */
+/* const section = document.body.appendChild(
+     document.createElement( 'section' )
+)
+
+p.onclick = function ( event ) {
+
+    ( arguments[0] && arguments[0].nodeType === 1 ?
+         arguments[0] : document.body )
+             .appendChild( this )
+                .style = `width: 100px`
+
+}.bind ( picture, section ) */

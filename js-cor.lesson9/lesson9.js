f17e84493e7f165143d20ef9eb4a116507de4cd5
goldenkamery <47122499+goldenkamery@users.noreply.github.com>
2019-04-07 08:26:04 +0300

    k2

--- js-cor.lesson9/lesson9.js
+++ js-cor.lesson9/lesson9.js
@@ -106,3 +106,38 @@ var users = (
 
 users.showAbsent()
 
+
+
+/* Additionally :thumbsup:
+Допилите код функции changeClass, которая парсит все стили страницы в поисках заданного класса, 
+а затем меняет атрибуты стиля этого класса:
+
+let changeClass = ( classname, styleString ) => ( 
+   ...
+).length > 0 ? console.log ( "found" ) : 
+    document.head.appendChild (
+        document.createElement ( "style" )
+    ).textContent = `.${classname} {${styleString}}`
+После вызова функции:
+
+changeClass ( "second-level-menu", "background-color: red!important;" )
+в консоли страницы, где есть элементы с классом second-level-menu, 
+у всех элементов этого класса цвет фона должен измениться на красный. */
+
+
+let changeClass = ( classname, styleString ) => ( Array.from ( document.styleSheets )
+    .filter ( sheet => !sheet.href )
+        .map (
+            sheet => Array.from ( sheet.cssRules )
+                    .filter ( rule => rule.selectorText === `.{classname}` )
+        )
+           .filter ( item => item.length > 0 )
+               .map ( item => item[0].cssText.split ("}")
+                           .join ( `${styleString}}` )
+               )
+).length > 0 ? console.log ( "found" ) : 
+    document.head.appendChild (
+        document.createElement ( "style" )
+    ).textContent = `.${classname} {${styleString}}`
+
+changeClass ( "second-level-menu", "background-color: red!important;" )

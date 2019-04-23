0831716fbbe50f9be26b18f31de5ca0894bb4224
goldenkamery <47122499+goldenkamery@users.noreply.github.com>
2019-04-23 13:00:47 +0300

    js7

--- js-cor.lesson10/lesson10.js
+++ js-cor.lesson10/lesson10.js
@@ -161,3 +161,21 @@ getKey = () => {
 } */
 
 
+function User ( name ) {
+    this.name = name
+    var presence = false
+    Object.defineProperty ( this, "presence", {
+        get: () =>
+            presence ? 
+                `${this.name} is present` : 
+                    `${this.name} is absent`,
+
+        set: newValue => presence = newValue 
+			 
+    })
+}
+
+let user = new User ( "Ivan" )
+console.info ( user.presence )
+user.presence = '+'
+console.info ( user.presence )
